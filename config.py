7ea6a51366c3f67176f3941a2bf326bb7509a0d5
Jibran <mjibrantariq246@gmail.com>
2024-07-10 18:14:51 +0700

    update dbconnection (test)

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = 'db_container'  # Updated to use the container name
     MYSQL_USER = 'user'
     MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'dbname'
-
-
