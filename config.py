0fe444eff94f281dffc705ceaa4e126c98e75368
Jibran <mjibrantariq246@gmail.com>
2024-07-10 17:49:06 +0700

    update config (test)

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = 'host.docker.internal'
     MYSQL_USER = 'user'
     MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'dbname'
