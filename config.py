d190524ea14cf3a409aac69a22a1fe3f8dbed563
Jibran <mjibrantariq246@gmail.com>
2024-07-11 00:37:24 +0700

    major update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    MYSQL_HOST = '174.138.27.151'  # Updated to use the container name
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'rootpassword'
+    MYSQL_HOST = 'dbcontainer'  # Updated to use the container name
+    MYSQL_USER = 'user'
+    MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'dbname'
