699ec7b76224d3dbf04f8989245728d6925fa7aa
Jibran <mjibrantariq246@gmail.com>
2024-07-10 18:10:46 +0700

    update app.py (test)

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    MYSQL_HOST = 'db'
+    MYSQL_HOST = 'localhost'
     MYSQL_USER = 'user'
     MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'dbname'
