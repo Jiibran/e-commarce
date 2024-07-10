735dfa67d09d11d882ddc8313abb8610bda2f20a
Jibran <mjibrantariq246@gmail.com>
2024-07-10 17:47:55 +0700

    update config (test)

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'ecommerce_db'
+    MYSQL_USER = 'user'
+    MYSQL_PASSWORD = 'password'
+    MYSQL_DB = 'dbname'
+
+
