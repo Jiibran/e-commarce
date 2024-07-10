5ea98b54c6aff17fcac1d8fb66ceaa8653b59806
Jibran <mjibrantariq246@gmail.com>
2024-07-11 02:03:02 +0700

    update CORS

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "http://174.138.27.151:8081"}})
 
 jwt = JWTManager(app)
 init_app(app)
